efe9d6ddb3a82aba1884140a06014b7f992f599e
Lukasz Raczylo <lukasz@raczylo.com>
2015-06-11 13:46:10 +0100

    New features
    - Take multiple arguments for project:
      It creates a single maintenance window with specified services included.
    - Filter:
      Find all the services matching name from --filter field.
    - Automated maintenance mode description contains creators system username.

--- pd-maintenance-mode.rb
+++ pd-maintenance-mode.rb
@@ -34,6 +34,11 @@ class PDMaintenance
   def self.pagerduty
     # List all the available PD projects together with their PD schedules IDs
     if $params.list_projects && ! $params.project_given
+      if $params.filter_given
+        @options[:query] = {
+          :query => $params.filter
+        }
+      end
       p_json = JSON.parse(self.get($pd_base_url + 'services', @options).body)
       table_data = Array.new
       longest_entry = 20
@@ -43,7 +48,7 @@ class PDMaintenance
           :created        => Chronic.parse(s['created_at']).ago.to_words,
           :status         => s['status'],
           :last_incident  => s['last_incident_timestamp'] == nil ? "never" : Chronic.parse(s['last_incident_timestamp']).ago.to_words,
-          :incidents      => s['incident_counts']['total']
+          :incidents      => s['incident_counts']['total'],
         }
         # checking for longest entry in the array of hashes to set appropriate table width
         tmp_data[:service_name].length > longest_entry ? longest_entry = tmp_data[:service_name].length : nil
@@ -51,10 +56,11 @@ class PDMaintenance
       end
       tp.set :max_width, longest_entry
       tp table_data
-    # Show info about PD schedule
+
     elsif $params.project_given && ! $params.time
+
       @options[:query] = {
-        "service_ids[]" => $params.project,
+        "service_ids" => $params.project,
         "include[]" => 'teams',
         "filter" => 'ongoing'
       }
@@ -64,7 +70,13 @@ class PDMaintenance
         table_data = Array.new
         p_json['maintenance_windows'].each do |mw|
           mw['description'].length > longest_entry ? longest_entry = mw['description'].length : nil
+          projects_included = Array.new
+          mw['services'].each do |pi|
+            projects_included.push(pi['id'])
+          end
           tmp_data = {
+            :maintenance_window      => mw['id'],
+            :projects     => projects_included.join(', '),
             :start_time   => Chronic.parse(mw['start_time']).ago.to_words,
             :end_time     => Chronic.parse(mw['end_time']),
             :description  => mw['description']
@@ -76,8 +88,10 @@ class PDMaintenance
       else
         puts "No active maintenance windows for specified project (#{$params.project})."
       end
+
+
     elsif $params.project_given && $params.time_given
-      description = $params.description_given ? $params.description : "Automated maintenance mode."
+      description = $params.description_given ? $params.description : "Automated maintenance mode ( created by: #{ENV['USER']} )"
       @options[:body] = {
         "maintenance_window" => {
           "service_ids"   => $params.project,
@@ -88,17 +102,21 @@ class PDMaintenance
       }.to_json
       p_json = JSON.parse(self.post($pd_base_url + "maintenance_windows", @options).body)
       if p_json['maintenance_window']
-        puts "Maintenance window #{p_json['maintenance_window']['id']} has been created for service #{$params.project}\nWindow starts #{Time.now} and expires #{Time.now + $params.time.to_i * 60}\nTo manage maintenance mode visit: https://#{ENV['PD_PROJECT_NAME']}.pagerduty.com/services/#{$params.project}"
+        puts "Maintenance window #{p_json['maintenance_window']['id']} has been created for service #{$params.project}\nWindow starts #{Time.now} and expires #{Time.now + $params.time.to_i * 60}"
+        $params.project.each do |p|
+         puts "To manage maintenance mode visit: https://#{ENV['PD_PROJECT_NAME']}.pagerduty.com/services/#{p}"
+        end
       end
     end
   end
 
   def self.parse
     opts = Trollop::options do
-      opt :project, "Pick specific PagerDuty project.", :type => :string
+      opt :project, "Pick specific PagerDuty service.", :type => :strings
       opt :time, "Enable maintenance window for specific time (in minutes).", :type => :string
-      opt :list_projects, "Shows list of the projects together with their ID"
+      opt :list_projects, "Shows list of the services together with their ID"
       opt :description, "Optional description for maintenance window.", :type => :string
+      opt :filter, "Search and apply for services matching the filter", :type => :string
     end
     return opts
   end
